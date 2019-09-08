fc64d168e90eeefe2cc2dae6537915f3224864eb
oyzq_glb <ouyangzhiqiang98_glb@gmail.com>
2019-09-08 22:37:57 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React ,{Component} from 'react';
 import Header from './common/header/index'
 import { Globalstyle } from './style';
-import store from './store'
-import { Provider } from 'react-redux'
-
+import store from './store';
+import { Provider } from 'react-redux';
+import { BrowserRouter,Route } from 'react-router-dom';
+ 
 class App extends Component {
   render() {
     return (
      <Provider store={store}>
        <Globalstyle/>
         <Header/>
+        <BrowserRouter>
+         <div>
+          <Route path='/' exact render={()=><div>home</div>}></Route>
+          <Route path='/detail' exact render={()=><div>detail</div>}></Route>
+         </div>
+        </BrowserRouter>
      </Provider>
     );
   }
